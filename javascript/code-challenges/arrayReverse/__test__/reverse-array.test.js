1664b8bc9c1be92180573822423c7544fb8d2906
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-03-28 19:29:12 +0300

    Testing code challenge-1

--- javascript/code-challenges/arrayReverse/__test__/reverse-array.test.js
+++ javascript/code-challenges/arrayReverse/__test__/reverse-array.test.js
@@ -1,6 +1,6 @@
 const reverseArray = require("../app.js") 
 
-describe("reversed", () => {
+describe("reverseArray", () => {
   test("It should return an array reversed ", () => {
     expect(reverseArray([1, 2, 3, 4, 5, 6])).toStrictEqual([6, 5, 4, 3, 2, 1]);
     expect(
